5ce18f291a3bd10d9593562ab9c50b53192016d8
Remigiusz Orlowski <remigiusz.orlowski@gmail.com>
2021-08-02 16:38:46 +0200

    Initial commit with a boilerplate

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="banner">
+      <h1>D.C. Visualize</h1>
+      <h2>Power of data visualization</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+</style>
